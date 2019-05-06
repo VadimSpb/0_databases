884a1ff4d48fe6213b4c66ce448f1cd5c909ed89
VadimSpb <43755763+VadimSpb@users.noreply.github.com>
2019-05-06 13:26:04 +0300

    Update and rename 03.01.sql to 03-01.sql

--- hw03/03-01.sql
+++ hw03/03-01.sql
@@ -2,3 +2,5 @@
 1. Пусть в таблице users поля created_at и updated_at оказались незаполненными. Заполните их текущими датой и временем.
 */
 
+UPDATE users SET created_at = NOW() WHERE created_at = '' OR created_at IS NULL
+UPDATE users SET created_at = NOW() WHERE created_at = '' OR created_at IS NULL
